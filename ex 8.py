166f0a1829bc25521768b5d485f5aeb46b65438f
julliete bezerra <julliete.bezerra@academico.ifpb.edu.br>
2018-12-23 20:40:54 -0300

    'mensagem'

---
+++ ex 8.py
@@ -0,0 +1,11 @@
+x=open('cadastro.txt','w')
+
+nome=input('digite nome: ')
+while nome!='':
+    sexo=input('digite sexo: ')
+    idade=input('digite idade: ')
+    x.write(nome+','+sexo+','+idade+'\n')
+    nome=input('digite nome: ')
+x.close()
+x=open('cadastro.txt','r')
+print(x.read())
